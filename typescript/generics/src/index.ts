6f58289d335ccfc5a08fadd04531c528185f5a31
Daniel Tapias Morales <tapmorales@gmail.com>
2021-02-18 22:37:08 +0000

    generics first example

---
+++ typescript/generics/src/index.ts
@@ -0,0 +1,25 @@
+console.log("______________")
+
+// function somaOuConcatena(x: number, y: number) {
+//     return x + y
+// }
+
+function somaOuConcatena(x: number | string, y: number | string) {
+    return (typeof x === "number" && typeof y === "number") ? x + y : x + "" + y
+}
+
+somaOuConcatena(1, 2)
+somaOuConcatena("1", "2")
+somaOuConcatena("1", 2)
+
+type NumberOrString = number | string
+
+function somaOuConcatenaG<T extends NumberOrString>(x: T, y: T) {
+    return (typeof x === "number" && typeof y === "number") ? x + y : x + "" + y
+}
+
+somaOuConcatenaG(1, 2)
+somaOuConcatenaG("1", "2")
+// somaOuConcatenaG(1, "2")         ERRO
+// somaOuConcatenaG("1", 2)         ERRO
+// somaOuConcatenaG(false, true)    ERRO
