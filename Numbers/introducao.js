859e3b953995ac4706c366a070ce32ef4fdebf32
Daniel Tapias Morales <tapmorales@gmail.com>
2021-03-31 20:27:03 +0100

    add isNaN

--- Numbers/introducao.js
+++ Numbers/introducao.js
@@ -39,3 +39,7 @@ console.log(numero.toLocaleString("pt-BR", { style: "currency", currency: "BRL" 
 console.log(numero.toLocaleString("pt-PT", { style: "currency", currency: "EUR" }))
 
 console.log(Number.MAX_VALUE, Number.MIN_VALUE)
+
+let numeroAsString = "a3.456"
+console.log(isNaN(numeroAsString))
+console.log(numeroAsString, typeof numeroAsString, parseFloat(numeroAsString))
